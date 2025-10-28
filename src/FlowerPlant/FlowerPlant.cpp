229c22b3bbe7a0723f988fa4c3e3b9f4ef2823e8
Ayush Sanjith <ayushsanjith@gmail.com>
2025-10-28 13:57:16 +0200

    Fix include path for CareVisitor header

--- src/FlowerPlant/FlowerPlant.cpp
+++ src/FlowerPlant/FlowerPlant.cpp
@@ -1,9 +1,10 @@
 #include "FlowerPlant.h"
-#include "src/CareVisitor/CareVisitor.h"
+#include "../CareVisitor/CareVisitor.h"
 
 FlowerPlant::FlowerPlant(const std::string& id, const std::string& name, 
                          std::shared_ptr<PlantData> data) : Plant(id, name, data) {}
 
 void FlowerPlant::accept(CareVisitor& visitor) {
     visitor.visitFlowerPlant(this);
+
 }
