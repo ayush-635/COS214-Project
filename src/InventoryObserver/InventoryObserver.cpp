9a9e61723c08bb3685663deb800cb00da19ba45d
Heinrich Romer <u23538181@tuks.co.za>
2025-10-28 08:22:47 +0200

    Changes to Observer pattern structure: - Added the ability to print stock list (printStock) within InventoryObserver.h (memory address followed by quantity) - changed notifyStockChange in Subject class from private to protected to ensure only derived Subject classes can trigger observer notifications

--- src/InventoryObserver/InventoryObserver.cpp
+++ src/InventoryObserver/InventoryObserver.cpp
@@ -1,6 +1,20 @@
 #include "InventoryObserver.h"
+#include <iostream>
 
-void InventoryObserver::onSotckChange(Plant* plant) {
-	// TODO - implement InventoryObserver::onSotckChange
-	throw "Not yet implemented";
+void InventoryObserver::onStockChange(Plant* plant) {
+	//not done, waiting on singleton
+	if(!plant) {
+		return;
+	}
+
+	stock[plant] = -1;
+	std::cout << "InventoryObserver: Stock changed for plant at " << plant << std::endl;
+}
+
+void InventoryObserver::printStock(std::ostream& os) const {
+	os << "InventoryObserver stock cache (" << stock.size() << " entries): \n";
+	std::map<Plant*, int>::const_iterator it = stock.begin();
+	for(;it != stock.end(); ++it){
+		os << " Plant@ " << it->first << "->" << it->second << "\n";
+	}
 }
