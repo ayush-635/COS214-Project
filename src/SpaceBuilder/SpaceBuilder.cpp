f6f9934fe1937413e8aa8d441241e8c7071f2d65
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-29 03:19:14 +0200

    Implemented SpaceBuilder.cpp

--- src/SpaceBuilder/SpaceBuilder.cpp
+++ src/SpaceBuilder/SpaceBuilder.cpp
@@ -1,31 +1,57 @@
 #include "SpaceBuilder.h"
+#include "PlantSpace.h"
+#include "SpaceComponent.h"
+#include <iostream>
+
+SpaceBuilder::SpaceBuilder() {
+    space = new PlantSpace();
+    space->name = "Unnamed Space";
+}
 
 void SpaceBuilder::setSize(int width, int length) {
-	// TODO - implement SpaceBuilder::setSize
-	throw "Not yet implemented";
+    // This is to Set the size for the next component to be added
+    if (space != nullptr) {
+        SpaceComponent* component = new SpaceComponent();
+        component->width = width;
+        component->length = length;
+        space->subspaces.push_back(component);
+        std::cout << "Set space size: " << width << "x" << length << std::endl;
+    }
 }
 
 void SpaceBuilder::reset() {
-	// TODO - implement SpaceBuilder::reset
-	throw "Not yet implemented";
+    // Delete current space and create a new one
+    if (space != nullptr) {
+        // Clean up subspaces
+        for (SpaceComponent* component : space->subspaces) {
+            delete component;
+        }
+        delete space;
+    }
+    space = new PlantSpace();
+    space->name = "Unnamed Space";
+    std::cout << "Builder reset" << std::endl;
 }
 
 void SpaceBuilder::setName(int name) {
-	// TODO - implement SpaceBuilder::setName
-	throw "Not yet implemented";
+	// Set the name of the space
+    if (space != nullptr) {
+        space->name = "Space_" + std::to_string(name);
+        std::cout << "Set space name to: " << space->name << std::endl;
+    }
 }
 
 void SpaceBuilder::addSubSpace() {
-	// TODO - implement SpaceBuilder::addSubSpace
-	throw "Not yet implemented";
+    // Add a default subspace component
+    if (space != nullptr) {
+        space->addComponent();
+    }
 }
 
 PlantSpace* SpaceBuilder::build() {
-	// TODO - implement SpaceBuilder::build
-	throw "Not yet implemented";
-}
-
-SpaceBuilder::SpaceBuilder() {
-	// TODO - implement SpaceBuilder::SpaceBuilder
-	throw "Not yet implemented";
+    // Return the built space and reset for next build
+    PlantSpace* result = space;
+    space = nullptr;  // Transfer ownership
+    std::cout << "Space built successfully" << std::endl;
+    return result;
 }
