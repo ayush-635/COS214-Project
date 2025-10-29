41d3e48fd4cdb480627042b6e5d19a8c98507d0d
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-29 03:04:43 +0200

    Implemented UpgradedStorage.cpp

--- src/UpgradedStorage/UpgradedStorage.cpp
+++ src/UpgradedStorage/UpgradedStorage.cpp
@@ -1,6 +1,15 @@
 #include "UpgradedStorage.h"
+#include <iostream>
+using namespace std;
 
 bool UpgradedStorage::refill() {
-	// TODO - implement UpgradedStorage::refill
-	throw "Not yet implemented";
+    cout << "[UpgradedStorage] Refilling upgraded storage (faster)..." << endl;
+    // Upgraded storage refills faster/more efficiently
+    return true;
+}
+
+bool UpgradedStorage::upgrade() {
+    cout << "[UpgradedStorage] Already at maximum upgrade level!" << endl;
+    // Already upgraded - cannot upgrade further
+    return false;
 }
