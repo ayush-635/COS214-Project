244dedc4f2f8c3383dce84603dc5eaf9594c1188
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-29 03:01:37 +0200

    Updated StorageState.cpp

--- src/StorageState/StorageState.cpp
+++ src/StorageState/StorageState.cpp
@@ -1,11 +1,3 @@
 #include "StorageState.h"
 
-bool StorageState::upgrade() {
-	// TODO - implement StorageState::upgrade
-	throw "Not yet implemented";
-}
-
-bool StorageState::refill() {
-	// TODO - implement StorageState::refill
-	throw "Not yet implemented";
-}
+// Pure virtual class -therefore no implementation needed
