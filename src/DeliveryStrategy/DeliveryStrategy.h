f71d10401523ea8a23fee559401408825b3545cc
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-29 03:28:11 +0200

    Updated DeliveryStrategy.h

--- src/DeliveryStrategy/DeliveryStrategy.h
+++ src/DeliveryStrategy/DeliveryStrategy.h
@@ -1,13 +1,17 @@
 #ifndef DELIVERYSTRATEGY_H
 #define DELIVERYSTRATEGY_H
 
+#include <iostream>
+#include <string>
+
+class Order;
+
 class DeliveryStrategy {
-
-
 public:
-	virtual void deliver(Order o) = 0;
-
-	DeliveryStrategy();
+    virtual void deliver(Order o) = 0;
+    virtual ~DeliveryStrategy() {}
+    
+    DeliveryStrategy();
 };
 
 #endif
