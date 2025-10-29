cc818ac13ddc04407ece8426afb59e74a446c26b
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-29 03:15:30 +0200

    Implemented PlantSpace.cpp

--- src/PlantSpace/PlantSpace.cpp
+++ src/PlantSpace/PlantSpace.cpp
@@ -1,11 +1,24 @@
 #include "PlantSpace.h"
+#include "SpaceComponent.h"
+#include <iostream>
 
 void PlantSpace::addComponent() {
-	// TODO - implement PlantSpace::addComponent
-	throw "Not yet implemented";
+    // Method to Add a default component to the plant space
+    SpaceComponent* newComponent = new SpaceComponent();
+    newComponent->width = 10;  // This is the Default width
+    newComponent->length = 10;  // And this is the Default length
+    subspaces.push_back(newComponent);
+    std::cout << "Added new component to " << name << std::endl;
 }
 
 void PlantSpace::removeComponent() {
-	// TODO - implement PlantSpace::removeComponent
-	throw "Not yet implemented";
+    // And a Method to Remove the last component if it exists
+    if (!subspaces.empty()) {
+        SpaceComponent* component = subspaces.back();
+        subspaces.pop_back();
+        delete component;
+        std::cout << "Removed component from " << name << std::endl;
+    } else {
+        std::cout << "No components to remove from " << name << std::endl;
+    }
 }
