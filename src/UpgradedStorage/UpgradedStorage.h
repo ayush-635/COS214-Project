7ad86912d4b9b90e76088c4dfab9003ffc43069e
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-31 13:29:45 +0200

    Updated UpgradedStorage.h

--- src/UpgradedStorage/UpgradedStorage.h
+++ src/UpgradedStorage/UpgradedStorage.h
@@ -1,12 +1,13 @@
 #ifndef UPGRADEDSTORAGE_H
 #define UPGRADEDSTORAGE_H
 
-#include "StorageState.h"
+#include "../State/StorageState.h"
+#include <string>
 
 class UpgradedStorage : public StorageState {
 public:
-    bool refill() override;
-    bool upgrade() override;
+    std::string refill() override;
+    std::string upgrade() override;
 };
 
 #endif
