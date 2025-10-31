fdfbc518cb083b726f18cf118aff292156d01dde
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-31 12:55:24 +0200

    Update StandardDelivery.cpp
    updated method signatures and changed cout to string

--- src/StandardDelivery/StandardDelivery.cpp
+++ src/StandardDelivery/StandardDelivery.cpp
@@ -1,16 +1,19 @@
 #include "StandardDelivery.h"
-#include "Order.h"
-#include <iostream>
+#include "../Order/Order.h"
+#include <string>
+#include <sstream>
 using namespace std;
 
-void StandardDelivery::deliver(Order o) {
-    cout << "=== STANDARD DELIVERY ===" << endl;
-    cout << "Delivery Method: Standard Shipping" << endl;
-    cout << "Estimated Delivery Time: 5-7 business days" << endl;
-    cout << "Order Total: R" << o.total() << endl;
-    cout << "Delivery Cost: R50.00" << endl;
-    cout << "Total Amount: R" << (o.total() + 50.00) << endl;
-    cout << "Package will be shipped via standard courier" << endl;
-    cout << "Tracking information will be sent via email" << endl;
-    cout << "=========================" << endl;
+string StandardDelivery::deliver(Order o) {
+    ostringstream oss;
+    oss << "=== STANDARD DELIVERY ===" << endl;
+    oss << "Delivery Method: Standard Shipping" << endl;
+    oss << "Estimated Delivery Time: 5-7 business days" << endl;
+    oss << "Order Total: R" << o.total() << endl;
+    oss << "Delivery Cost: R50.00" << endl;
+    oss << "Total Amount: R" << (o.total() + 50.00) << endl;
+    oss << "Package will be shipped via standard courier" << endl;
+    oss << "Tracking information will be sent via email" << endl;
+    oss << "=========================" << endl;
+    return oss.str();
 }
