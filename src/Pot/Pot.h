9b9c422570cf1cce6bb0b2cadf881598ab3bfe8f
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2025-10-31 11:37:41 +0200

    Enhance Pot class with price and getters
    Added price attribute and corresponding getter methods.

--- src/Pot/Pot.h
+++ src/Pot/Pot.h
@@ -4,13 +4,17 @@
 #include <string>
 
 class Pot {
-
 private:
-	std::string colour;
-	int size;
+    std::string colour;
+    int size;
+    double price;
 
 public:
-	Pot(std::string c, int size);
+    Pot(std::string c, int size, double price);
+    
+    std::string getColour() const { return colour; }
+    int getSize() const { return size; }
+    double getPrice() const { return price; }
 };
 
 #endif
