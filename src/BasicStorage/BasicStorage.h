ca7d0ac3b3e824d8d762b98832132dcfcff4b825
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-31 13:46:07 +0200

    Update BasicStorage.h

--- src/BasicStorage/BasicStorage.h
+++ src/BasicStorage/BasicStorage.h
@@ -2,12 +2,11 @@
 #define BASICSTORAGE_H
 
 #include "../State/StorageState.h"
-#include <string>
 
 class BasicStorage : public StorageState {
 public:
-    std::string refill() override;
-    std::string upgrade() override;
+    bool refill() override;
+    bool upgrade() override;
 };
 
 #endif
