5247370e58f6880ae5cf600143b724f1485570bd
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-10-31 13:45:36 +0200

    Update StorageState.h

--- src/StorageState/StorageState.h
+++ src/StorageState/StorageState.h
@@ -1,15 +1,13 @@
 #ifndef STORAGESTATE_H
 #define STORAGESTATE_H
 
-#include <string>
-
 class StorageState {
 public:
 //made pure virtual-we dont want to use multiple inheritance here
     virtual ~StorageState() {}
     
-    virtual std::string upgrade() = 0;
-    virtual std::string refill() = 0;
+    virtual bool upgrade() = 0;
+    virtual bool refill() = 0;
 };
 
 #endif
