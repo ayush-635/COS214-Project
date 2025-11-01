7ca8690c0e54475fee77446eaa0de84ae7fed678
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2025-11-01 07:23:37 +0200

    Enhance HerbPlant class with documentation comments
    Added detailed documentation comments for the HerbPlant class and its methods.

--- src/HerbPlant/HerbPlant.h
+++ src/HerbPlant/HerbPlant.h
@@ -3,15 +3,38 @@
 
 #include "../Plant/Plant.h"
 
+/**
+ * @class HerbPlant
+ * @brief Concrete plant class representing herb-type plants
+ */
 class HerbPlant : public Plant {
 public:
+    /**
+     * @brief Construct a new Herb Plant object
+     * @param id Unique identifier for the plant
+     * @param name Display name of the plant
+     * @param data Shared plant data configuration
+     */
     HerbPlant(const std::string& id, const std::string& name, 
               std::shared_ptr<PlantData> data);
-	HerbPlant(const HerbPlant& other) : Plant(other) { }
+    
+    /**
+     * @brief Copy constructor
+     * @param other The HerbPlant to copy from
+     */
+    HerbPlant(const HerbPlant& other) : Plant(other) { }
 
+    /**
+     * @brief Accept a care visitor
+     * @param visitor The care visitor to accept
+     */
     void accept(CareVisitor& visitor) override;
 
-	virtual Plant* clone();
+    /**
+     * @brief Create a clone of this plant
+     * @return Plant* Pointer to the cloned plant
+     */
+    virtual Plant* clone() override;
 };
 
 #endif
