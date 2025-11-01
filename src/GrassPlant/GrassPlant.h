dc9a1b3cb4ae9b3c8fb77a30bb52bab46ec8a3e4
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2025-11-01 07:18:26 +0200

    Enhance GrassPlant class with documentation comments
    Added detailed documentation comments for the GrassPlant class and its methods.

--- src/GrassPlant/GrassPlant.h
+++ src/GrassPlant/GrassPlant.h
@@ -3,14 +3,37 @@
 
 #include "../Plant/Plant.h"
 
+/**
+ * @class GrassPlant
+ * @brief Concrete plant class representing grass-type plants
+ */
 class GrassPlant : public Plant {
 public:
+    /**
+     * @brief Construct a new Grass Plant object
+     * @param id Unique identifier for the plant
+     * @param name Display name of the plant
+     * @param data Shared plant data configuration
+     */
     GrassPlant(const std::string& id, const std::string& name, 
                std::shared_ptr<PlantData> data);
+    
+    /**
+     * @brief Copy constructor
+     * @param other The GrassPlant to copy from
+     */
     GrassPlant(const GrassPlant& other) : Plant(other) { }
-	
+    
+    /**
+     * @brief Accept a care visitor
+     * @param visitor The care visitor to accept
+     */
     void accept(CareVisitor& visitor) override;
 
+    /**
+     * @brief Create a clone of this plant
+     * @return Plant* Pointer to the cloned plant
+     */
     virtual Plant* clone() override;
 };
 
