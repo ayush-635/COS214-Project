a6a79e4789c732dccb68ed1d615982adca72f4f5
JanoVenter-UP <u22496409@tuks.co.za>
2025-11-01 21:51:26 +0200

    Added doc strings

--- src/TreeFactory/TreeFactory.h
+++ src/TreeFactory/TreeFactory.h
@@ -5,6 +5,16 @@
 
 class TreeFactory : PlantFactory {
 public:
+
+	/*
+	 * @brief Creates a new tree plant instance by cloning the prototype associated with the given name.
+	 * 
+	 * @param name The name of the tree plant prototype to clone.
+	 * 
+	 * @return Pointer to the newly created tree plant instance.
+	 * 
+	 * @note Implements the createPlant method from the PlantFactory base class.
+	 */
 	virtual Plant* createPlant(std::string name) override;
 };
 
