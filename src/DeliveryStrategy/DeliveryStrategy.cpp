be35419f8b8838eaf5d682bf5018bf8203692083
infamouszay <130178061+infamouszay@users.noreply.github.com>
2025-11-01 23:45:34 +0200

    Added Doxygen documentation to DeliveryStrategy.cpp

--- src/DeliveryStrategy/DeliveryStrategy.cpp
+++ src/DeliveryStrategy/DeliveryStrategy.cpp
@@ -1,5 +1,17 @@
+/**
+ * @file DeliveryStrategy.cpp
+ * @brief Implementation of DeliveryStrategy base class
+ * @author Zaynab Samir
+ */
+
 #include "DeliveryStrategy.h"
 
+/**
+ * @brief Constructor implementation
+ * 
+ * Base constructor for the abstract DeliveryStrategy class.
+ * No initialization needed as this is an interface class.
+ */
 DeliveryStrategy::DeliveryStrategy() {
     // Constructor implementation (if needed) nothing here to initialize(Base Constructor)
 }
