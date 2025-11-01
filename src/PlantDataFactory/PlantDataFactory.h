b9c545f653d2d468498d4f65facfec1e6541ff4d
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2025-11-01 07:31:57 +0200

    Add documentation to PlantDataFactory class
    Added documentation comments for the PlantDataFactory class and its methods.

--- src/PlantDataFactory/PlantDataFactory.h
+++ src/PlantDataFactory/PlantDataFactory.h
@@ -5,14 +5,26 @@
 #include <map>
 #include <memory>
 
+/**
+ * @class PlantDataFactory
+ * @brief Factory for creating and accessing plant data configurations
+ */
 class PlantDataFactory {
 private:
-    static std::map<std::string, std::shared_ptr<PlantData>> plantDataMap;
+    static std::map<std::string, std::shared_ptr<PlantData>> plantDataMap; ///< Registry of plant data
 
 public:
+    /**
+     * @brief Get plant data for a specific species
+     * @param key The species name to look up
+     * @return std::shared_ptr<PlantData> Shared pointer to plant data, or nullptr if not found
+     */
     static std::shared_ptr<PlantData> getPlantData(const std::string& key);
+    
+    /**
+     * @brief Initialize the factory with all plant species data
+     */
     static void initializeFactory();
 };
-
 
 #endif
