7d2792038686dea58dec4073bce0c8ad84896d49
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2025-11-01 07:37:25 +0200

    Enhance TreePlant.h with documentation comments
    Added documentation comments for TreePlant class and its methods.

--- src/TreePlant/TreePlant.h
+++ src/TreePlant/TreePlant.h
@@ -3,15 +3,38 @@
 
 #include "../Plant/Plant.h"
 
+/**
+ * @class TreePlant
+ * @brief Concrete plant class representing tree-type plants
+ */
 class TreePlant : public Plant {
 public:
+    /**
+     * @brief Construct a new Tree Plant object
+     * @param id Unique identifier for the plant
+     * @param name Display name of the plant
+     * @param data Shared plant data configuration
+     */
     TreePlant(const std::string& id, const std::string& name, 
                 std::shared_ptr<PlantData> data);
+    
+    /**
+     * @brief Copy constructor
+     * @param other The TreePlant to copy from
+     */
     TreePlant(const TreePlant& other) : Plant(other) { }
 
+    /**
+     * @brief Accept a care visitor
+     * @param visitor The care visitor to accept
+     */
     void accept(CareVisitor& visitor) override;
-	
-	virtual Plant* clone() override;
+    
+    /**
+     * @brief Create a clone of this plant
+     * @return Plant* Pointer to the cloned plant
+     */
+    virtual Plant* clone() override;
 };
 
 #endif
