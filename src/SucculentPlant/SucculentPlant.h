5e708566b7c5e2af1a47c487fdecc959801414a1
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2025-11-01 07:35:57 +0200

    Enhance SucculentPlant.h with documentation
    Added documentation comments for class and methods.

--- src/SucculentPlant/SucculentPlant.h
+++ src/SucculentPlant/SucculentPlant.h
@@ -3,14 +3,38 @@
 
 #include "src/Plant/Plant.h"
 
+/**
+ * @class SucculentPlant
+ * @brief Concrete plant class representing succulent-type plants
+ */
 class SucculentPlant : public Plant {
 public:
+    /**
+     * @brief Construct a new Succulent Plant object
+     * @param id Unique identifier for the plant
+     * @param name Display name of the plant
+     * @param data Shared plant data configuration
+     */
     SucculentPlant(const std::string& id, const std::string& name, 
                    std::shared_ptr<PlantData> data);
-	SucculentPlant(const SucculentPlant& other) : Plant(other) { }
+    
+    /**
+     * @brief Copy constructor
+     * @param other The SucculentPlant to copy from
+     */
+    SucculentPlant(const SucculentPlant& other) : Plant(other) { }
 
-	void accept(CareVisitor& visitor) override;
-	virtual Plant* clone() override;
+    /**
+     * @brief Accept a care visitor
+     * @param visitor The care visitor to accept
+     */
+    void accept(CareVisitor& visitor) override;
+    
+    /**
+     * @brief Create a clone of this plant
+     * @return Plant* Pointer to the cloned plant
+     */
+    virtual Plant* clone() override;
 };
 
 #endif
