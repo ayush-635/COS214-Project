fccc53a58140cd54b1224b9cd7bd032c070eb720
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2025-11-01 07:30:24 +0200

    Enhance PlantData class with documentation
    Added documentation comments for class and methods.

--- src/PlantData/PlantData.h
+++ src/PlantData/PlantData.h
@@ -1,20 +1,23 @@
-
 #ifndef PLANTDATA_H
 #define PLANTDATA_H
 #include <string>
 
+/**
+ * @class PlantData
+ * @brief Configuration data container for plant species
+ */
 class PlantData {
 private:
-	std::string speciesName;
-	double basePrice;
-	int wateringNeed;
-	int fertilizerNeed;
-	double growthRate;
-	bool outside;
-	bool lowLight;
-	bool lowWater;
-	bool brightColour;
-	bool lowCare;
+    std::string speciesName;
+    double basePrice;
+    int wateringNeed;
+    int fertilizerNeed;
+    double growthRate;
+    bool outside;
+    bool lowLight;
+    bool lowWater;
+    bool brightColour;
+    bool lowCare;
     int waterNeededForNextState;
     int fertilizerNeededForNextState;
     int waterInterval;
@@ -23,11 +26,26 @@ private:
     int fertilizerDeathTime;
 
 public:
+    /**
+     * @brief Construct a new Plant Data object
+     * @param name Species name
+     * @param waterForState Water needed for state progression
+     * @param fertilizerForState Fertilizer needed for state progression
+     * @param waterInt Water interval in ticks
+     * @param fertilizerInt Fertilizer interval in ticks
+     * @param waterDeath Time without water until death
+     * @param fertilizerDeath Time without fertilizer until death
+     */
     PlantData(const std::string& name, int waterForState, int fertilizerForState,
               int waterInt, int fertilizerInt, int waterDeath, int fertilizerDeath);
     
+    /**
+     * @brief Copy constructor
+     * @param other The PlantData to copy from
+     */
     PlantData(const PlantData& other);
 
+    // Getters
     std::string getSpeciesName() const { return speciesName; }
     int getWaterNeededForNextState() const { return waterNeededForNextState; }
     int getFertilizerNeededForNextState() const { return fertilizerNeededForNextState; }
@@ -40,6 +58,11 @@ public:
     bool isLowWater() const { return lowWater; }
     bool isBrightColour() const { return brightColour; }
     bool isLowCare() const { return lowCare; }
+    
+    /**
+     * @brief Get pointer to this plant data
+     * @return PlantData* Pointer to this plant data instance
+     */
     PlantData* getPlantData(){return this;};
 };
 
